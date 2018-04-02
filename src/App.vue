b9527848097b7e8a4ce91f6ee4dd33d2836d1408
Kanta Paul <pobon.paul1994@gmail.com>
2018-04-03 00:35:09 +0600

    add blank file

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,20 @@
 <template>
   <div id="app">
-    <div class="container">
-      <h1> {{ massage }} </h1>
-      <contact-us></contact-us>
-      <contact-us></contact-us>
-      <counter></counter>
-      <about></about>
-    </div>
+
   </div>
 </template>
 
 <script>
-// local component example
-var aboutUs = {
-  data () {
-    return {
-      counter: 0
-    }
-  },
-  template: `
-    <div>
-      <h2>Hi i am local component</h2>
-      <button class="btn btn-primary" @click="counter++"> {{ counter }} </button>
-    </div>
-  `
-}
-
 export default {
   name: 'App',
-  components: {
-    'about': aboutUs
-  },
   data () {
     return {
-      massage: 'Welcome to my page'
+      
     }
   }
 }
 </script>
 
 <style>
-body {
-  font-size: 14px;
-  font-weight: 400;
-  color: #333333;
-  overflow-x: hidden;
-  font-family: 'Ubuntu', sans-serif;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -6,36 +6,6 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-// global component example
-Vue.component('contact-us', {
-  data () {
-    return {
-      email: 'mail@example.com'
-    }
-  },
-  template: `
-    <div>
-      <h1>Hello's world</h1>
-      <p>Please contact us {{ email }}</p>
-    </div>
-  `
-})
-
-// global component example
-Vue.component('counter', {
-  data () {
-    return {
-      counter: 0
-    }
-  },
-  template: `
-    <div>
-      <h2>Hi i am global component</h2>
-      <button class="btn btn-primary" @click="counter++"> {{ counter }} </button>
-    </div>
-  `
-})
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
